02d67338b67576c8c49ffc5c83bb07e0108df104
Dominic Galiano <dgaliano@utexas.edu>
2025-04-23 16:44:05 -0400

    Add dummy api calling function

--- static/script.js
+++ static/script.js
@@ -1,12 +1,16 @@
 const map = L.map("map").setView([19, 0], 2);
 
+let lat = 0;
+let lng = 0;
 let circle_area = null;
 let radius = 0; // km
+let population = 0; // result
 
-let radius_slider = document.getElementById("radius-slider");
-let radius_display = document.getElementById("radius-value");
+const radius_slider = document.getElementById("radius-slider");
+const radius_display = document.getElementById("radius-value");
 const lat_display = document.getElementById("lat");
 const lng_display = document.getElementById("lng");
+const result_display = document.getElementById("result");
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -20,24 +24,41 @@ window.onload = function () {
   radius_display.textContent = radius;
 };
 
+function onAreaChange() {
+  lat_display.textContent = lat;
+  lng_display.textContent = lng;
+  radius_display.textContent = radius;
+
+  if (!circle_area) {
+    circle_area = L.circle([lat, lng], { radius: radius * 1000 }).addTo(map);
+    return;
+  }
+
+  circle_area.setLatLng([lat, lng]);
+  circle_area.setRadius(radius * 1000);
+
+  getAreaPopulation();
+}
+
 function onSliderChange(_) {
   radius = radius_slider.value;
   radius_display.textContent = radius;
 
-  if (circle_area) {
-    circle_area.setRadius(radius * 1000);
-  }
+  // Too many UI updates on slider, just redraw with new radius on next click
+  // onAreaChange();
 }
 
 function onMapClick(e) {
-  lat_display.textContent = e.latlng.lat;
-  lng_display.textContent = e.latlng.lng;
+  lat = e.latlng.lat;
+  lng = e.latlng.lng;
+  onAreaChange();
+}
 
-  if (!circle_area) {
-    circle_area = L.circle(e.latlng, { radius: radius * 1000 }).addTo(map);
-  } else {
-    circle_area.setLatLng(e.latlng);
-  }
+function getAreaPopulation() {
+  let population_result = Math.floor(Math.random() * 100_000 + 1);
+
+  population = population_result;
+  result_display.textContent = population_result;
 }
 
 radius_slider.addEventListener("input", onSliderChange, false);
