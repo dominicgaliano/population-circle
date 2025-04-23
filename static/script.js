092d921eab2c6f3ec46d5ecd6aeed47a941db1a7
Dominic Galiano <dgaliano@utexas.edu>
2025-04-23 15:51:23 -0400

    Add circle without changeable radius

--- static/script.js
+++ static/script.js
@@ -1,7 +1,23 @@
-console.log("Hello, world");
-const map = L.map("map").setView([20, 0], 2);
+const map = L.map("map").setView([19, 0], 2);
+let circle_area = null;
+let radius = 10; // km
+const lat_display = document.getElementById("lat");
+const lng_display = document.getElementById("lng");
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+function onMapClick(e) {
+  lat_display.textContent = e.latlng.lat;
+  lng_display.textContent = e.latlng.lng;
+
+  if (!circle_area) {
+    circle_area = L.circle(e.latlng, { radius: radius * 1000 }).addTo(map);
+  } else {
+    circle_area.setLatLng(e.latlng);
+  }
+}
+
+map.on("click", onMapClick);
